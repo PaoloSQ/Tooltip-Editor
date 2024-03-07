701fbe97e1c1e036c0e3967f593bbfbb825a247f
PaoloSQ <ben281909@gmail.com>
2024-03-07 11:34:24 +0000

    fix: Se arregló el error de re-pintar la figura tras hacer un nuevo punto. - Se modificó la lógica, ahora en vez de hacer un punto dandolé coordenadas, se obtenedrán las coordenadas al hacer un click, se guardarán y finalmente  estas se visualizarán en forma de punto. - Después de dibujar los puntos, se calculará el área y se pintará.

--- pruebas/app.js
+++ pruebas/app.js
@@ -12,17 +12,41 @@ canvas.addEventListener("click", (event) => {
   const rect = canvas.getBoundingClientRect();
   const x = event.clientX - rect.left;
   const y = event.clientY - rect.top;
-  vertices.push({ x, y });
-  dibujarPunto(x, y);
+  const clickedIndex = getClickedPointIndex(x, y);
+  if (clickedIndex !== -1) {
+    vertices.splice(clickedIndex, 1);
+  } else {
+    vertices.push({ x, y });
+  }
+  dibujarPuntos();
   calcularArea();
 });
 
+// Obtener la coordenada del click dentro de la imagen.
+function getClickedPointIndex(x, y) {
+  for (let i = 0; i < vertices.length; i++) {
+    const distance = Math.sqrt(
+      (x - vertices[i].x) ** 2 + (y - vertices[i].y) ** 2
+    );
+    if (distance <= 5) {
+      // Radio del punto de clic
+      return i;
+    }
+  }
+  return -1;
+}
+
 // Dibuja un punto rojo en las coordenadas clickeadas
-function dibujarPunto(x, y) {
-  ctx.fillStyle = "red";
-  ctx.beginPath();
-  ctx.arc(x, y, 5, 0, 2 * Math.PI);
-  ctx.fill();
+function dibujarPuntos() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.drawImage(backgroundImage, 0, 0, canvas.width, canvas.height);
+  for (let i = 0; i < vertices.length; i++) {
+    const { x, y } = vertices[i];
+    ctx.fillStyle = "red";
+    ctx.beginPath();
+    ctx.arc(x, y, 5, 0, 2 * Math.PI);
+    ctx.fill();
+  }
 }
 
 // Calcula área utilizando el método de Shoelace
@@ -34,10 +58,13 @@ function calcularArea() {
       ctx.lineTo(vertices[i].x, vertices[i].y);
     }
     ctx.closePath();
-    ctx.fillStyle = "rgba(255, 0, 0, 0.5)";
+    ctx.fillStyle = "rgba(255, 0, 0, 0.5)"; // Color rojo con opacidad
     ctx.fill();
     info.innerHTML = "Área coloreada";
   } else {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.drawImage(backgroundImage, 0, 0, canvas.width, canvas.height);
+    dibujarPuntos();
     info.innerHTML = "Necesitas al menos 3 vértices para formar un polígono.";
   }
 }
